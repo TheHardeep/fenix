2933b52fec93020d32323a7cca9b5fc96cb00cdd
TheHardeep <hardeep.hd13@gmail.com>
2023-12-09 09:56:04 +0530

    Added Kunjee Broker.

---
+++ kunjee.py
@@ -0,0 +1,25 @@
+from kronos.symphony import symphony
+
+
+class kunjee(symphony):
+    """
+    Kunjee kronos Broker Class
+
+    Returns:
+        kronos.kunjee: kronos Kunjee Broker Object
+    """
+
+
+    # Market Data Dictonaries
+
+    id = 'kunjee'
+
+
+    # Base URLs
+
+    base_urls = {
+        "api_documentation_url": "https://trade.kunjee.net:3000/doc/interactive",
+        "market_data_url": "https://trade.kunjee.net:3000/marketdata/instruments/master",
+        "base_url": "https://trade.kunjee.net:3000/interactive",
+        "access_token": "https://trade.kunjee.net:3000/interactive/user/session"
+    }

8729413bd2ee1c04b6ee90011474aaf6837b801f
TheHardeep <hardeep.hd13@gmail.com>
2023-12-09 09:56:22 +0530

    Added VPC Broker.

---
+++ vpc.py
@@ -0,0 +1,25 @@
+from kronos.symphony import symphony
+
+
+class vpc(symphony):
+    """
+    VPC kronos Broker Class
+
+    Returns:
+        kronos.vpc: kronos VPC Broker Object
+    """
+
+
+    # Market Data Dictonaries
+
+    id = 'vpc'
+
+
+    # Base URLs
+
+    base_urls = {
+        "api_documentation_url": "http://122.160.19.15:3000/doc/interactive",
+        "market_data_url": "http://122.160.19.15:3000/marketdata/instruments/master",
+        "base_url": "http://122.160.19.15:3000/interactive",
+        "access_token": "http://122.160.19.15:3000/interactive/user/session"
+    }
